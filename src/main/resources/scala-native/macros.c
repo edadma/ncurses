37610eeb8f939a25fb9df99068e1a7d1e44e8e1b
Edward A Maxedon, Sr <edadma@gmail.com>
2021-08-16 00:22:05 -0400

    add functions

--- src/main/resources/scala-native/macros.c
+++ src/main/resources/scala-native/macros.c
@@ -1,9 +1,14 @@
 #include <ncurses.h>
 
 int ncurses_refresh() { return refresh(); }
-
 int ncurses_getch() { return getch(); }
-
 int ncurses_move(int y, int x) { return move(y, x); }
-
 int ncurses_addstr(char* str) { return addstr(str); }
+int ncurses_KEY_DOWN() { return KEY_DOWN; }
+int ncurses_KEY_UP() { return KEY_UP; }
+int ncurses_KEY_LEFT() { return KEY_LEFT; }
+int ncurses_KEY_RIGHT() { return KEY_RIGHT; }
+int ncurses_KEY_HOME() { return KEY_HOME; }
+int ncurses_KEY_BACKSPACE() { return KEY_BACKSPACE; }
+int ncurses_KEY_DC() { return KEY_DC; }
+int ncurses_clrtoeol() { return clrtoeol(); }
--- src/main/scala/xyz/hyperreal/ncurses/LibNCurses.scala
+++ src/main/scala/xyz/hyperreal/ncurses/LibNCurses.scala
@@ -1,6 +1,6 @@
 package xyz.hyperreal.ncurses
 
-import scala.scalanative.unsafe.{CInt, CString, CVarArgList, Ptr, extern, link, name}
+import scala.scalanative.unsafe.{CBool, CInt, CString, CVarArgList, Ptr, extern, link, name}
 @link("ncurses")
 @extern
 object LibNCurses {
@@ -17,6 +17,8 @@ object LibNCurses {
   def vw_printw(win: _win_st, fmt: CString, args: CVarArgList): CInt = extern
   def wmove(win: _win_st, y: CInt, x: CInt): CInt                    = extern
   def waddnstr(win: _win_st, str: CString, n: CInt): CInt            = extern
+  def keypad(win: _win_st, bf: CBool): CInt                          = extern
+  def wclrtoeol(win: _win_st): CInt                                  = extern
 
   @name("ncurses_refresh")
   def refresh: CInt = extern
@@ -26,5 +28,20 @@ object LibNCurses {
   def move(y: CInt, x: CInt): CInt = extern
   @name("ncurses_addstr")
   def addstr(str: CString): CInt = extern
-
+  @name("ncurses_KEY_DOWN")
+  def KEY_DOWN: CInt = extern
+  @name("ncurses_KEY_UP")
+  def KEY_UP: CInt = extern
+  @name("ncurses_KEY_LEFT")
+  def KEY_LEFT: CInt = extern
+  @name("ncurses_KEY_RIGHT")
+  def KEY_RIGHT: CInt = extern
+  @name("ncurses_KEY_HOME")
+  def KEY_HOME: CInt = extern
+  @name("ncurses_KEY_BACKSPACE")
+  def KEY_BACKSPACE: CInt = extern
+  @name("ncurses_KEY_DC")
+  def KEY_DC: CInt = extern
+  @name("ncurses_clrtoeol")
+  def clrtoeol: CInt = extern
 }
