8bd371c397adcd9a43ba1815430035e12a8f57f3
Edward A Maxedon, Sr <edadma@gmail.com>
2021-08-19 06:43:21 -0400

    fix macros.c

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "ncurses"
 
-version := "0.1.0-snapshot.28"
+version := "0.1.0-snapshot.29"
 
 scalaVersion := "2.13.6"
 
--- src/main/resources/scala-native/macros.c
+++ src/main/resources/scala-native/macros.c
@@ -6,7 +6,7 @@ int ncurses_move(int y, int x) { return move(y, x); }
 int ncurses_addstr(char* str) { return addstr(str); }
 int ncurses_mvaddstr(int y, int x, char* str) { return mvaddstr(y, x, str); }
 int ncurses_waddstr(WINDOW* win, char* str) { return waddstr(win, str); }
-int ncurses_mvwaddstr(WINDOW* win, int y, int x, char* str) { return waddstr(win, y, x, str); }
+int ncurses_mvwaddstr(WINDOW* win, int y, int x, char* str) { return mvwaddstr(win, y, x, str); }
 int ncurses_clrtoeol() { return clrtoeol(); }
 int ncurses_clrtobot() { return clrtobot(); }
 int ncurses_getmaxy(WINDOW* win) { return getmaxy(win); }
